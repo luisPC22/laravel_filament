ae46810f054822df7f1da8bfdffe6ae44e210107
luisPC22 <luispillaca.1998@gmail.com>
2024-10-13 11:09:03 -0500

    feat: create migrations and models for project

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,9 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                // change active color dashboard
+                'primary' => Color::Red,
+                'gray' => Color::Slate
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
